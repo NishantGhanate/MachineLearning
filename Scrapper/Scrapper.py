9b9435b6af4d11e68b65c7df6e29915b13fca8e8
NishantGhanate <nishant7.ng@gmail.com>
2018-02-22 20:31:04 +0530

    Scrappy 1st

--- Scrapper/Scrapper.py
+++ Scrapper/Scrapper.py
@@ -7,9 +7,40 @@ Created on Mon Feb 19 16:40:42 2018
 
 import scrapy
 
-class FirstSpider(scrapy.Spider): 
-   name = "first" 
-   
-   def __init__(self, group = None, *args, **kwargs): 
-      super(FirstSpider, self).__init__(*args, **kwargs) 
-      self.start_urls = ["http://www.example.com/group/%s" % group]
+class TechcrunchSpider(scrapy.Spider):
+    #name of the spider
+    name = 'techcrunch'
+
+    #list of allowed domains
+    allowed_domains = ['techcrunch.com/feed/']
+
+    #starting url for scraping
+    start_urls = ['http://techcrunch.com/feed/']
+
+    #setting the location of the output csv file
+    custom_settings = {
+        'FEED_URI' : 'tmp/techcrunch.csv'
+    }
+
+    def parse(self, response):
+        #Remove XML namespaces
+        response.selector.remove_namespaces()
+
+        #Extract article information
+        titles = response.xpath('//item/title/text()').extract()
+        authors = response.xpath('//item/creator/text()').extract()
+        dates = response.xpath('//item/pubDate/text()').extract()
+        links = response.xpath('//item/link/text()').extract()
+
+        for item in zip(titles,authors,dates,links):
+            scraped_info = {
+                'title' : item[0],
+                'author' : item[1],
+                'publish_date' : item[2],
+                'link' : item[3]
+            }
+
+            yield scraped_info
+            
+            
+            
---
+++ Scrapper/sample/sample/spiders/Test.py
@@ -0,0 +1,15 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
+class MySpider(BaseSpider):
+	name = "Nishant" #my spider name 
+	allowed_domains = ["craiglist.org"]
+	start_urls = ["https://sfbay.craiglist.org/sfc/npo/"]
+	
+	def parse(self,response):
+		hxs = HtmlXPathSelector(response)
+		titles = hxs.select("//p")
+		for titles in titles:
+			title = titles.select("a/text()").extract()
+			link = title.select("a/@href").extract()
+			print (title , link )
