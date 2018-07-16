788bf08a8b6ac54f6e6171af9f164569ce74ccf1
nishantghanate <nishant7.ng@gmail.com>
2018-07-16 23:53:54 +0530

    channel issue

--- Keras/Cifar10.py
+++ Keras/Cifar10.py
@@ -5,20 +5,27 @@ from keras.utils import np_utils # utilities for one-hot encoding of ground trut
 import numpy as np
 from matplotlib import pyplot as plt
 from keras import backend as K
+from keras.models import Sequential
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
 
 batch_size = 32 # in each iteration, we consider 32 training examples at once
 num_epochs = 200 # we iterate 200 times over the entire training set
 kernel_size = 3 # we will use 3x3 kernels throughout
 pool_size = 2 # we will use 2x2 pooling throughout
-conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
-conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
-drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
-drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
-hidden_size = 512 # the FC layer will have 512 neurons
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+
+
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data() # fetch CIFAR-10 data
 
-# print(X_train.shape) #(50000, 32, 32, 3)
+print("X train = " + X_train.shape ) #(50000, 32, 32, 3)
+
+# num_train, height, width, depth = X_train.shape # there are 50000 training examples in CIFAR-10 
+# num_test = X_test.shape[0] # there are 10000 test examples in CIFAR-10
+# num_classes = np.unique(y_train).shape[0] # there are 10 image classes
 
 # for i in range(0, 9):
 # 	plt.subplot(330 + 1 + i)
@@ -29,11 +36,11 @@ hidden_size = 512 # the FC layer will have 512 neurons
 # (n, depth, width, height). a full-color image with all 3 RGB channels will have a depth of 3.
 if K.image_data_format() == 'channels_first':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = x_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    X_train = x_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = x_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
 
@@ -43,7 +50,7 @@ X_test = X_test.astype('float32')
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
-print('x_train shape:', x_train.shape)
+print('x_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
@@ -52,8 +59,32 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
+# Create the model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+# Compile model
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
 
-# num_train, height, width, depth = X_train.shape # there are 50000 training examples in CIFAR-10 
-# num_test = X_test.shape[0] # there are 10000 test examples in CIFAR-10
-# num_classes = np.unique(y_train).shape[0] # there are 10 image classes
+
+
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
 
