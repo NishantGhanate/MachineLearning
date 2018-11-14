070c12932524bf3ef2ed9d11416a9a73512f3b74
nishantghanate <nishant7.ng@gmail.com>
2018-11-14 17:38:07 +0530

    DL - building and training ANN

--- DeepLearningA-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/BankPrediction_AAN.py
+++ DeepLearningA-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/BankPrediction_AAN.py
@@ -37,15 +37,13 @@ import pandas as pd
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 
-# @DataPreprocssing removing unwated independent variables
+# ---------------@DataPreprocssing removing unwated independent variables------
     
 # Selecting coulmns from index 3 to 12 since upper bound is ignored we use +1 index 
 X = dataset.iloc[:,3:13].values
 
 # Selecting last index containing binray 1 = yes will bank or 0 = no 
 y = dataset.iloc[:,13].values
-
-
 
 # OneHotEncoder : https://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
 # LabelEncoder , OneHotEncoder:  http://www.stephacking.com/encode-categorical-data-labelencoder-onehotencoder-python/
@@ -71,4 +69,50 @@ from sklearn.model_selection import train_test_split
 X_train , X_test , y_train , y_test = train_test_split(X,y,test_size = 0.2 , random_state = 0)
 
 # Feature scaling : https://www.geeksforgeeks.org/python-how-and-where-to-apply-feature-scaling/
+from sklearn.preprocessing import StandardScaler 
+# Initialise the Scaler 
+scaler = StandardScaler() 
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
 
+# -------------------@Building ANN import keras library-----------------------
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initliazing ANN Model 
+classifier = Sequential()
+
+""" too look params select Dense and press ctrl + i
+    units = Hidden layer = 6 : we have 11 input & 1 output = 12/2 = 6
+    inputdim = no.of input / Independent varibales
+    kernel_initializer = assigning wieghts to our Neural network close to zero 
+    Activation functions : https://towardsdatascience.com/activation-functions-neural-networks-1cbd9f8d91d6
+"""
+# First Layer must req no of input
+classifier.add(Dense(units = 6 ,  kernel_initializer = "uniform", activation = "relu" , input_dim=11)) 
+
+# Adding second hidden layer
+classifier.add(Dense(units = 6 ,  kernel_initializer = "uniform", activation = "relu" )) 
+
+# Final layer has only 1 output i.e either custmer will bank yes or no
+# if there is multiple outout add  units = no. of outout and activation = "softmax"
+classifier.add(Dense(units = 1 ,  kernel_initializer = "uniform", activation = "sigmoid" )) 
+
+""" @Complining model
+    Adam : https://machinelearningmastery.com/adam-optimization-algorithm-for-deep-learning/
+"""  
+classifier.compile(optimizer = "adam" , loss = "binary_crossentropy" , metrics = ["accuracy"])
+
+# Fitting dataset into our model 
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100 )
+
+# y_pred = Predticiting whether a customer will leave bank % 
+y_pred = classifier.predict(X_test)
+
+# converting % value to True or value threshold = 50%
+y_pred = (y_pred > 0.5)
+
+# Making Confusion matrix to validate prediction from testing result 
+from sklearn.metrics import  confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
